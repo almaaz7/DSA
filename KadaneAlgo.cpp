56bea93272b2d7756edc63cbc176cc479d0870fa
almaaz7 <almaazahmed825@gmail.com>
2023-07-10 22:54:47 +0530

    kadanes brute force

--- KadaneAlgo.cpp
+++ KadaneAlgo.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 
 using namespace std;
-
+// optimal Kadans algo
 int solve(vector<int> &arr)
 {
     int sum = 0;
@@ -17,10 +17,26 @@ int solve(vector<int> &arr)
     }
     return maxi;
 }
+//brute force
 
+int solve3(vector<int> arr){
+    int sum =0;
+    int maxi = INT_MIN;
+    for(int i=0;i<arr.size();i++){
+        for(int j =i;j<arr.size();j++){
+            sum = 0;
+            for(int k=i;k<j;k++){
+                sum = sum + arr[k]; 
+                maxi = max(sum,maxi);  
+            }
+        }
+    }
+    return maxi;
+}
 int main()
 {
     vector<int> arr = {-2,1,-3,4,-1,2,1,-5,4};
-    int ans = solve(arr);
+    int ans = solve3(arr);
     cout<<"ans is "<<ans;
 }
+
